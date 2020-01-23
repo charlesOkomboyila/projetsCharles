2993b2e86fe3fe67dc82294c76de043889845ea1
Jerome <jerome@sallmann.fr>
2020-01-23 13:13:53 +0100

    Correction de la requête permettant de récupérer toutes les BD. Le cas des multi-auteurs a été traité.

--- application/models/bdmodel.php
+++ application/models/bdmodel.php
@@ -7,10 +7,12 @@ class bdModel extends CI_Model
     public static function getAll()
     {
         $CI = get_instance();
-        $query = $CI->db->select("bd.titre as titre, bd.prix_public as prix, a.nom as auteur, ref as couverture")
+        $query = $CI->db->select("bd.titre as titre, bd.prix_public as prix, GROUP_CONCAT(a.nom SEPARATOR ' / ') as auteur, ref as couverture")
         ->from("bd")
         ->join("auteur_bd","bd.id=auteur_bd.bd_id")
         ->join("auteur a", "auteur_bd.auteur_id=a.id")
+        ->order_by("titre")
+        ->group_by("titre")
         ->get();
 
         $result = $query->result();
